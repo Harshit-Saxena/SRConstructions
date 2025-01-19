cb97ae3db38704725bf2dcb1f56e470879793a40
Pulkit-s21 <pulkitsaxena795@gmail.com>
2025-01-19 12:39:45 +0530

    Btn Added to page

--- src/components/Proprietor.jsx
+++ src/components/Proprietor.jsx
@@ -72,6 +72,33 @@ const Proprietor = () => {
           </p>
         </div>
       </div>
+
+      <button
+        className="fixed bottom-6 right-6 w-14 h-14 bg-white rounded-full shadow-lg flex items-center justify-center z-30 transform hover:scale-110 hover:bg-highlight hover:text-white hover:shadow-2xl transition-all"
+        onClick={() => {
+          window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+          })
+        }}
+      >
+        <div className="flex items-center justify-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-8 h-8"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth="2"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M5 15l7-7 7 7"
+            />
+          </svg>
+        </div>
+      </button>
     </section>
   )
 }
